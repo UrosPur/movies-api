30941d060363baae69d5a1210f5c7d37fe20f033
UrosPurtic <uros.purtic@gmail.com>
2018-05-21 21:02:06 +0200

    movies mogration seeder

---
+++ database/factories/MovieFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\Movie::class, function (Faker $faker) {
+
+    return [
+        'title' => $faker->realText(10),
+        'director' => $faker->firstNameMale,
+        'imageUrl' => $faker->imageUrl(),
+        'duration' => $faker->randomNumber(4),
+        'releaseDate' => $faker->date('Y-m-d'),
+        'genre' =>  $faker->title
+    ];
+});
